3cc9370b172f425bc11f2c6ddbfdbe46d3efadf3
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-18 00:12:07 -0400

    Color is now updating properly

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Square = ({ colorName }) => {
+  const css = colorName;
+  console.log(css);
+
   return (
     // The div for the square
-    <div className='square' style={{backgroundColor: {colorName}}}>
+    <div className='square' style={{backgroundColor: css}}>
       {/* Ternary operator to display the default text or the color name */}
       { colorName ? <p>{colorName}</p> : <p>Empty Value</p> }
     </div>
