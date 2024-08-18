5c10e0d183b53b60f107371fd2d7ff3cfc598d7f
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-18 00:13:36 -0400

    Added clarifying comments

--- src/Components/ColorChange.js
+++ src/Components/ColorChange.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 const ColorChange = ({ colorName, setColorName }) => {
   return (
+    // Form for updating the color name
     <form className='colorForm'>
         <label htmlFor='colorName'>Change Color</label>
+
+        {/* the input */}
         <input 
             autofocus
             id='colorName'
             placeholder='Add Color Name'
             required
+
+            // set the colorName state to the value
             value={colorName}
+
+            // update the state anytime the value changes
             onChange={(e) => setColorName(e.target.value)}
         />
     </form>
