ec3ca19a61a486e8d9b53498d9fee1d5701a201c
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-17 23:47:17 -0400

    Applied inline styling for the color

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Square = () => {
   return (
     // The div for the square
-    <div className='square'>
+    <div className='square' style={{backgroundColor: "red"}}>
       {/* Ternary operator to display the default text or the color name */}
       {}
       Empty Value
